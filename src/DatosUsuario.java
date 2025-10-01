3eaa4058e888f3f22d7a990c611fe0e3512cd5e8
Jorgesantiago11 <jrgsantiagoflores@gmail.com>
2025-10-01 14:16:13 +0200

    Update DatosUsuario.java

--- src/DatosUsuario.java
+++ src/DatosUsuario.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 import utilidades.ProcesadorTexto;
+import utilidades.Usuario;
 
 public class DatosUsuario {
     public static void main(String[] args) {
@@ -13,19 +14,39 @@ public class DatosUsuario {
         String name = sc.nextLine();
         System.out.println("Introduce tu edad");
         int edad = sc.nextInt();
+        sc.nextLine();
+
         System.out.println("Introduce tu correo electrónico");
         String correo = sc.nextLine();
 
         // Cerrar Scanner
         sc.close();
 
-        // Comrpobación del correo
-        boolean correoValido = ProcesadorTexto.esEmailValido(correo);
+        // Condición nombre
+        if (name.length() > 2 || name.length() < 20) {
 
-        if (correoValido) {
-            System.out.println("Tu correo existe");
         } else {
-            System.out.println("Tu correo no existe");
+            System.out.println("Error en el nombre");
+
+            // Condición edad
+            if (edad > 0 || edad < 120) {
+
+            } else {
+                System.out.println("Error en la edad");
+
+            }
+
+            // Comrpobación del correo
+            boolean correoValido = ProcesadorTexto.esEmailValido(correo);
+
+            if (correoValido) {
+                Usuario usuario = new Usuario(name, edad, correo);
+                usuario.mostrarInformacion();
+                System.out.println("El usuario ha sido creado");
+            } else {
+                System.out.println("El usuario no puede ser creado porque el correo introducido no es válido");
+            }
+
         }
 
     }
